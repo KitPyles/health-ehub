b2e4de1145d69ee5c48f4e9a798f4d3465515734
KitPyles <pyles.kit@gmail.com>
2023-03-03 22:54:21 -0600

    Initial Project Commit

---
+++ src/main/java/com/healthehub/healthehub/HealthEhubApplication.java
@@ -0,0 +1,13 @@
+package com.healthehub.healthehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthEhubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthEhubApplication.class, args);
+    }
+
+}
